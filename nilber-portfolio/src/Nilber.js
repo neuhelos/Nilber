5e81f754d0fd7cb8e398061fabf0b56ea07e4e31
Nilber Remon <nilberremon@gmail.com>
2020-09-23 19:40:52 -0400

    Header Styling

--- nilber-portfolio/src/Nilber.js
+++ nilber-portfolio/src/Nilber.js
@@ -1,25 +1,42 @@
 import React from 'react';
 
-import { ThemeProvider } from "@material-ui/core/styles";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import { theme } from "./Styling/ThemeProvider.js";
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { makeStyles } from '@material-ui/core/styles';
+import { theme } from './Styling/ThemeProvider.js';
+
+import Typography from "@material-ui/core/Typography"
+import './Styling/TwinklingStarClouds.css'
 
 import BioGrid from './Components/BioGrid'
 
-import './Styling/TwinklingStarClouds.css'
+const useStyles = makeStyles( (theme) => ({
+    root: {
+      '& *': {
+        fontFamily: 'jost'
+      }
+    },
+    text: {
+      fontFamily: 'jost'
+    }
+
+}))
+
 
 const Nilber = () => {
   
+  const classes = useStyles()
+
   return(
 
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <div class="stars"></div>
-      <div class="twinkling"></div>
-      <div class="clouds"></div>  
       <div className="App">
+        <div className="stars"></div>
+        <div className="twinkling"></div>
+        <div className="clouds"></div>  
+        <Typography className={classes.text} align='center' variant="h1">Nilber Remon</Typography>
         <BioGrid />
-
       </div>
     </ThemeProvider>
 

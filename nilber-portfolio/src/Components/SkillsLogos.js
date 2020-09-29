e4ecb164299ce9e1edce453875270db378e74f28
Nilber Remon <nilberremon@gmail.com>
2020-09-28 20:59:18 -0400

    Skills Additions

--- nilber-portfolio/src/Components/SkillsLogos.js
+++ nilber-portfolio/src/Components/SkillsLogos.js
@@ -9,16 +9,21 @@ import FirebaseLogo from '../Styling/Images/FirebaseLogo.svg'
 import SQLLogo from '../Styling/Images/SQLDatabaseIcon.svg'
 import NoSQLLogo from '../Styling/Images/NoSQLLogo.svg'
 import MaterialUILogo from '../Styling/Images/MaterialUILogo.svg'
+import ExpressLogo from '../Styling/Images/ExpressJSLogo.svg'
+import StyledComponentsLogo from '../Styling/Images/Styled-ComponentsLogo.svg'
 
 export const skillsLogos = [
     {skill: 'JavaScript', logo: JavascriptLogo},
     {skill: 'React', logo: ReactLogo},
     {skill: 'Redux', logo: ReduxLogo},
     {skill: 'Node', logo: NodeLogo},
+    {skill: 'Express', logo: ExpressLogo},
     {skill: 'HTML', logo: HTML5Logo},
     {skill: 'CSS', logo: CSS3Logo},
     {skill: 'Firebase', logo: FirebaseLogo},
-    {skill: 'SQL & NoSQL', logo: SQLLogo},
+    {skill: 'SQL', logo: SQLLogo},
+    {skill: 'NoSQL', logo: NoSQLLogo},
     {skill: 'Git', logo: GitLogo},
-    {skill: 'MaterialUI', logo: MaterialUILogo}
+    {skill: 'MaterialUI', logo: MaterialUILogo},
+    {skill: 'S-C', logo: StyledComponentsLogo},
 ]

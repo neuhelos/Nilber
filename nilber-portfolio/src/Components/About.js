9a6ba1bedc0ac4a23bcddeebbb8d9923295569d8
Nilber Remon <nilberremon@gmail.com>
2020-10-06 17:56:22 -0400

    About Image Update

--- nilber-portfolio/src/Components/About.js
+++ nilber-portfolio/src/Components/About.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import Nilber from '../Styling/Images/NilberRemonCroppedCircle.png'
+import Nilber from '../Styling/Images/NilberRedBlueStar.png'
 
 import { makeStyles } from '@material-ui/core/styles'
 import Box from '@material-ui/core/Box'

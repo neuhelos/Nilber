9e935d362a07feeb5fef980eda7302c6f98524e6
Nilber Remon <nilberremon@gmail.com>
2020-12-29 04:29:43 -0500

    Resume Timeline Posts

--- nilber-portfolio/src/Components/ResumeTimeline/ResumePosts.js
+++ nilber-portfolio/src/Components/ResumeTimeline/ResumePosts.js
@@ -13,10 +13,50 @@ export const projectPosts = [
         organization: "ZONE 126 - FUND FOR THE CITY OF NY",
         title: "Data & Evaluation Manager",
         responsibilities: [
-            "Lead bi-weekly in-depth study conversations with 5-6 fellows on front-end, UI/UX design and styling"
+            "Coordinate data analytics and data collection logistics across sites, programs and organizational partners.",
+            "Provide data quality assurance management and perform and design quality controls",
+            "Propose recommendations and implement protocols to streamline data processes, including establishing standard operating procedures (SOP)  and data management policies.",
+            "Manage data entry staff and provide ongoing oversight of their performance.",
+            "Oversee organizational database management and assist in analyzing and reporting data.",
         ]
-    }
-
-
-
+    },
+    {
+        dates: "FEB 2016 - DEC 2018",
+        organization: "AIDS HEALTHCARE FOUNDATION",
+        title: "Associate Director of Quality & Operations",
+        responsibilities: [
+            `Managed the national operations, logistics, purchasing and procurement, quality assurance, property, fleet and facilities matters, 
+            administrative structures and processes of PHD, directing cross-regional PHD Operations Managers and Staff (10) that provided 
+            service coverage domestically across the US.`,
+            "Oversaw the national development and expansion of PHD Operations’ scope and infrastructure",
+            "Managed, developed and forecasted the PHD Operations department budget of $1.3 million.",
+            "Supported the strategic direction of the organization designing centralized SOPs and policies.",
+            "Oversee organizational database management and assist in analyzing and reporting data.",
+            `Provided cross-functional management in coordinating the ongoing integration of departments, functions 
+            and platforms through the alignment of standards, models and initiatives while building peer 
+            relationships and consensus across the organization; Provided continual reporting and evaluation to PHD 
+            Leadership`,
+            `Managed and conducted site/facility quality assurance and operational visits to all PHD Sites and 
+            Programs as well as providing Operations-driven training and development to PHD Management and 
+            Staff nationally.`       
+        ]
+    },
+    {
+        dates: "MAR 2013 - JAN 2016",
+        organization: "AIDS HEALTHCARE FOUNDATION",
+        title: "Operations Manager",
+        responsibilities: [
+            `Managed the day-to-day operations, logistics and quality assurance structures and processes of 
+            AHF’s Public Health Division for all Programs and Sites in the Eastern US, including inventory 
+            management, distribution and logistics  and office administrative and facilities and properties 
+            management and functions.`,
+            `Managed and coordinated data reporting and analysis, including deliverables reporting for grant funding for multiple 
+            sites and programs. Generated and compiled reporting on organizational metrics and performance.`,
+            "Assisted in directing PHD’s procurement and purchasing activities and managing vendor relationships.",
+            `Managed and developed PHD’s national resource management and order fulfillment system for supplying site and 
+            program needs and tracking resource utilization.`,
+            `Developed and coached PHD Operations Teams based on strength and needs assessment and organizational capacity-building; 
+            helped to develop talent acquisition tools and an onboarding program.`
+        ]
+    },
 ]

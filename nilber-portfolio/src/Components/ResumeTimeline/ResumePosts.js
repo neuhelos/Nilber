c056e573b127c8996fbbfcdd8034169dd1430ce8
nilberremon <nilberremon@gmail.com>
2026-03-10 23:00:23 -0400

    Updated Job Description Resume Posts

--- nilber-portfolio/src/Components/ResumeTimeline/ResumePosts.js
+++ nilber-portfolio/src/Components/ResumeTimeline/ResumePosts.js
@@ -1,129 +1,145 @@
 export const resumePosts = [
-    {
-        dates: "JAN 2025 - JAN 2027",
-        organization: "Google Cloud Professional Data Engineer Certificate",
-        link: "https://www.credly.com/badges/6c3283e3-e13c-4cfd-bd01-270bc4544c91",
-        responsibilities: []
-    },
-    {
-        dates: "FEB 2021 - SEPT 2025",
-        organization: "ADSWERVE",
-        link: "https://adswerve.com",
-        title: "Data Engineer 2",
-        responsibilities: [
-            {
-                title: "Enterprise Solution Delivery",
-                description: "Served as a primary technical consultant for enterprise clients, leading cross-functional teams on enterprise-scale projects, architecting and delivering solutions for strategic business initiatives and POCs. Led discovery sessions and guided clients through the adoption of complex cloud and data ecosystems. Translated technical architectures into clear business value, enabling non-technical stakeholders to leverage their data effectively. For example, I designed and developed a cloud-based solution scaling and streamlining web analytics and social media targeting implementations across thousands of web properties."
-            },
-            {
-                title: "Stakeholder Engagement and Education",
-                description: "Provided cloud and data engineering knowledge expertise and training to enterprise clients and internal stakeholders. Supported clients with solutions and practices that enabled hundreds of thousands in cost savings for cloud and data infrastructure and services. Optimized automated SQL queries and SQL workflows resulting in thousands of dollars in savings for large-scale clients."
-            },
-            {
-                title: "Cloud Automation & Data Ecosystem Design",
-                description: "Devised, built and automated data architectures, batch and streaming ELT/ETL pipelines and orchestration workflows to centralize and standardize marketing, analytics and client data enabling unified reporting and advanced data modeling work. Solutions that processed, transformed and transferred daily from thousands to millions of streamed and/or batched data points/records. Architected and developed an event-driven data warehouse hub ecosystem for an enterprise client with pipelines to nearly two dozen disparate data sources ingesting millions of total records daily."
-            },
-            {
-                title: "AI Implementation",
-                description: "Partnered with data scientists to deliver predictive, generative AI, agentic and machine learning models and solutions. Supported the design and development of LLM-powered, RAG-enhanced and agent-driven Gen AI applications and tooling. These solutions helped provide clients with a 85% increase in return on ad spend while reducing cost 35% per conversion."
-            },
-            {
-                title: "Product Development",
-                description: "Built reusable, modular applications and internal tooling, developing solutions through an end-to-end agile software development lifecycle - from technical design to testing, deployment, and delivery. Rigorously pursued automation of tasks and processes to increase productivity. Containerized and productized recurrent client use cases, including data migration across clouds."
-            },
-            {
-                title: "Cloud-Native Microservices Architecture",
-                description: "Architected and deployed scalable, API-first microservices (REST/GraphQL) on cloud platforms using Python. Defined engineering standards for service-oriented architecture, resulting in a 30% reduction in time-to-market and eliminated long-term technical debt."
-            },
-            {
-                title: "Cloud Infrastructure",
-                description: "Applied IaC practices in solutions that automated cloud resources and services provisioning for greater reliability and reusability, reducing development time for the engineering team by 80% (hundreds of labor hours) and slashing errors and bugs by 75%."
-            },
-            {
-                title: "Data Activation",
-                description: "Engineered, managed and maintained data warehouse and reporting solutions facilitating advanced analytics, attribution modeling, marketing activation and business intelligence decision-making (Tableau, Sigma). Across projects accelerated client insights and achievement of conversion and sales goals."
-            },
-            {
-                title: "Data Analytics",
-                description: "Built and supported web analytics and data measurement implementations (Google Marketing Platform, Adobe Experience Platform) on websites and web apps serving up to millions of daily users. Optimized data fidelity and identity stitching, driving conversion goals up by 35%."
-            },
-            {
-                title: "UI/UX Design",
-                description: "Designed immersive and responsive front-end apps and interfaces (React, Next.js, Streamlit) with AI integrations, enhancing stakeholder and user engagement and experience."
-            }
-        ]
-    },
-    {
-        dates: "JAN 2020 - JAN 2021",
-        organization: "PURSUIT",
-        link: "https://pursuit.org",
-        title: "Front-End Web Development Study Lead",
-        responsibilities: [
-            {
-                title: "Front-end Instruction",
-                description: "Facilitated in-depth study sessions on front-end web development. Guided fellows through hands-on projects, enhancing their practical skills and understanding of front-end tech."
-            },
-            {
-                title: "Mentorship",
-                description: "Provided mentorship office hours on front-end development, UI/UX design and web frameworks."
-            }
-        ]
-    },
-    {
-        dates: "JAN 2019 - FEB 2021",
-        organization: "ZONE 126 - FUND FOR THE CITY OF NY",
-        link: "https://zone126.org",
-        title: "Data & Evaluation Manager",
-        responsibilities: [
-            {
-                title: "Data Governance & Quality Assurance",
-                description: "Designed and enforced organizational data quality controls and integrity protocols. Oversaw the end-to-end data lifecycle, ensuring accuracy and reliability for critical internal reporting and stakeholder analysis."
-            },
-            {
-                title: "Strategic Data Operations",
-                description: "Directed data analytics and reporting initiatives, transforming raw data into actionable insights to drive organizational strategy. Established key performance indicators (KPIs) to measure program efficacy and operational success."
-            },
-            {
-                title: "Team Leadership & Performance Management",
-                description: "Managed and mentored a team of data evaluation staff. Implemented metric-based performance tracking to optimize team output, foster professional development and ensure alignment with organizational goals."
-            },
-            {
-                title: "Process Standardization",
-                description: "Developed and implemented comprehensive Standard Operating Procedures (SOPs) to streamline data ingestion and processing workflows. Reduced manual redundancy and improved the speed and consistency of data deliverables."
-            }
-        ]
-    },
-    {
-        dates: "MAR 2013 - DEC 2018",
-        organization: "AIDS HEALTHCARE FOUNDATION",
-        link: "https://aidshealth.org",
-        title: "Associate Director of Quality & Operations",
-        responsibilities: [
-            {
-                title: "Enterprise Operations Leadership",
-                description: "Directed national operations and quality assurance for a major public health organization. Designed and scaled enterprise-wide operational architectures across logistics, procurement, assets and facilities, ensuring seamless service delivery at a national scale."
-            },
-            {
-                title: "Data Analytics & Governance",
-                description: "Oversaw the end-to-end data lifecycle for nationwide organizational data, including designing data quality controls. Analyzed organizational datasets to identify friction points in service delivery and leveraged these insights to drive process improvements and correct regional deviations, ensuring high-quality patient care and 100% regulatory compliance across jurisdictions."
-            },
-            {
-                title: "Workflow Optimization & Process Design",
-                description: "Re-engineered legacy operational workflows to eliminate bottlenecks and reduce latency in service delivery. Established and standardized national SOPs and processes to ensure consistency and reliability across distributed regional teams."
-            },
-            {
-                title: "Organizational Change Management",
-                description: "Led diverse, cross-regional teams through complex operational pivots and policy updates, fostering a culture of adaptability and resilience during high-pressure public health scenarios."
-            },
-            {
-                title: "Fiscal Governance & Resource Allocation",
-                description: "Orchestrated a $1.3M annual operating budget, implementing rigorous auditing protocols to maximize resource efficiency and maintain strict compliance with complex governmental grant requirements."
-            },
-            {
-                title: "Cross-Functional Stakeholder Leadership",
-                description: "Bridged the gap between clinical, administrative, and government stakeholders. Translated complex regulatory constraints into actionable operational strategies and led national training initiatives to upskill management and staff."
-            }
-        ]
-    }
-]
-
+  {
+    dates: "JAN 2025 - JAN 2027",
+    organization: "Google Cloud Professional Data Engineer Certificate",
+    link: "https://www.credly.com/badges/6c3283e3-e13c-4cfd-bd01-270bc4544c91",
+    responsibilities: [],
+  },
+  {
+    dates: "FEB 2021 - SEPT 2025",
+    organization: "ADSWERVE",
+    link: "https://adswerve.com",
+    title: "Data Engineer 2",
+    responsibilities: [
+      {
+        title: "Enterprise Solution Delivery",
+        description:
+          "Served as a primary technical consultant for enterprise clients, leading cross-functional teams on enterprise-scale projects. Led the full project lifecycle end-to-end from discovery sessions, technical design and planning, creating POCs to enabling clients to adopt complex cloud and data solutions. Translated technical architectures into clear business value, enabling non-technical stakeholders to leverage their data effectively. For example, I designed and developed a cloud-based solution scaling and streamlining web analytics and social media targeting implementations across thousands of web properties.",
+      },
+      {
+        title: "Client Engagement and Education",
+        description:
+          "Provided cloud and data engineering knowledge expertise and training to enterprise clients and internal stakeholders. Developed documentation, technical and troubleshooting resources for empowering solution end users. Supported clients with solutions and practices that enabled hundreds of thousands in cost savings for cloud and data infrastructure and services.",
+      },
+      {
+        title: "Cloud Automation & Data Ecosystem Design",
+        description:
+          "Devised, built and automated data architectures, batch and streaming ELT/ETL pipelines and orchestration workflows to centralize and standardize marketing, analytics and client data enabling unified reporting and advanced data modeling work. Owned the end-to-end transformation lifecycle for solutions that transferred and processed daily from thousands to millions of data points/records. Architected and developed an event-driven data warehouse hub ecosystem for an enterprise client with pipelines to over a dozen disparate data sources ingesting millions of total records daily.",
+      },
+      {
+        title: "Data Optimization, Quality, Validation & Observability",
+        description:
+          "Optimized SQL queries and workflows resulting in thousands of dollars in savings for large-scale clients. Implemented validation and automated quality checks in data solutions that reduced discrepancies by 30%, ensuring high fidelity for critical reporting. Established robust observability, implementing logging, alerting and monitoring to assess platform health. This initiative reduced mean-time-to-resolution (MTTR) for pipeline failures and ensured solution stability.",
+      },
+      {
+        title: "AI Implementation",
+        description:
+          "Partnered with data scientists to deliver predictive, generative AI, agentic and machine learning models and solutions. Supported the design and development of LLM-powered, RAG-enhanced and agent-driven Gen AI applications and tooling. These solutions helped provide clients with a 85% increase in return on ad spend while reducing cost 35% per conversion.",
+      },
+      {
+        title: "Product Development",
+        description:
+          "Built reusable, modular applications and internal tooling, developing solutions through an end-to-end agile software development lifecycle - from technical design to testing, deployment, and delivery. Rigorously pursued automation of tasks and processes to increase productivity. Containerized and productized recurrent client use cases, including data migration across clouds.",
+      },
+      {
+        title: "Cloud-Native Microservices Architecture",
+        description:
+          "Architected and deployed scalable, API-first microservices (REST/GraphQL) on cloud platforms using Python that handled heavy data loads. Defined engineering standards for service-oriented architecture, resulting in a 30% reduction in time-to-market and eliminated long-term technical debt.",
+      },
+      {
+        title: "Cloud Infrastructure",
+        description:
+          "Applied IaC practices in solutions that automated cloud resources and services provisioning for greater reliability and reusability, reducing development time for the engineering team by 80% (hundreds of labor hours) and slashing errors and bugs by 75%.",
+      },
+      {
+        title: "Data Enablement",
+        description:
+          "Engineered, managed and maintained data warehouse and reporting solutions facilitating advanced analytics, attribution modeling, marketing activation and business intelligence decision-making (Tableau, Sigma). Across projects accelerated client insights and achievement of conversion and sales goals.",
+      },
+      {
+        title: "Data Analytics & Tracking",
+        description:
+          "Configured complex tracking stacks across platforms. Implemented tagging and server-side tracking (sGTM) for Meta CAPI, Google Ads and other major ad networks, ensuring compliant measurement with consent management on websites and web apps serving up to millions of daily users. Optimized data fidelity and identity stitching, driving conversion goals up by 35% for clients. Debugged measurement and attribution discrepancies.",
+      },
+      {
+        title: "UI/UX Design",
+        description:
+          "Designed immersive and responsive front-end apps and interfaces (React, Next.js, Streamlit) with AI integrations, enhancing stakeholder and user engagement and experience.",
+      },
+    ],
+  },
+  {
+    dates: "JAN 2020 - JAN 2021",
+    organization: "PURSUIT",
+    link: "https://pursuit.org",
+    title: "Front-End Web Development Study Lead",
+    responsibilities: [
+      {
+        title: "Front-end Instruction",
+        description:
+          "Facilitated in-depth study sessions on front-end web development. Guided fellows through hands-on projects, enhancing their practical skills and understanding of front-end tech.",
+      },
+      {
+        title: "Mentorship",
+        description:
+          "Provided mentorship office hours on front-end development, UI/UX design and web frameworks.",
+      },
+    ],
+  },
+  {
+    dates: "JAN 2019 - FEB 2021",
+    organization: "ZONE 126 - FUND FOR THE CITY OF NY",
+    link: "https://zone126.org",
+    title: "Data & Evaluation Manager",
+    responsibilities: [
+      {
+        title: "Data Governance, Operations & Quality Assurance",
+        description:
+          "Directed analytics and reporting initiatives and designed and enforced organizational data quality controls. Oversaw the end-to-end data lifecycle, transforming data into actionable insights to drive organizational strategy and ensuring reliability for critical internal reporting. Established KPIs to measure program efficacy and operational success.",
+      },
+      {
+        title: "Team Leadership & Performance Management",
+        description:
+          "Managed and mentored a team of data evaluation staff. Implemented metric-based performance tracking to optimize team output, foster professional development and ensure alignment with organizational goals.",
+      },
+      {
+        title: "Process Standardization",
+        description:
+          "Developed and implemented comprehensive Standard Operating Procedures (SOPs) to streamline data ingestion and operational workflows, reducing manual redundancies.",
+      },
+    ],
+  },
+  {
+    dates: "MAR 2013 - DEC 2018",
+    organization: "AIDS HEALTHCARE FOUNDATION",
+    link: "https://aidshealth.org",
+    title: "Associate Director of Quality & Operations",
+    responsibilities: [
+      {
+        title: "Enterprise Operations Leadership",
+        description:
+          "Directed national operations and quality assurance for a major public health organization, managing cross-regional teams. Designed and scaled enterprise-wide operational architectures across logistics, procurement, assets and facilities, ensuring seamless service delivery. Navigated teams through complex operational pivots, fostering a culture of adaptability and resilience.",
+      },
+      {
+        title: "Data Analytics & Governance",
+        description:
+          "Oversaw the end-to-end data lifecycle for nationwide organizational data, including designing data quality controls. Analyzed organizational datasets to identify friction points in service delivery and leveraged these insights to drive process improvements and correct regional deviations, ensuring high-quality patient care and 100% regulatory compliance across jurisdictions.",
+      },
+      {
+        title: "Workflow Optimization & Process Design",
+        description:
+          "Re-engineered legacy operational workflows to eliminate bottlenecks and reduce latency in service delivery. Established and standardized national SOPs and processes to ensure consistency and reliability across distributed regional teams.",
+      },
+      {
+        title: "Fiscal Governance & Resource Allocation",
+        description:
+          "Orchestrated a $1.3M annual operating budget, implementing rigorous auditing protocols to maximize resource efficiency and maintain strict compliance with complex governmental grant requirements.",
+      },
+      {
+        title: "Cross-Functional Stakeholder Leadership",
+        description:
+          "Bridged the gap between clinical, administrative and government stakeholders. Translated complex regulatory constraints into actionable operational strategies and led national training initiatives to upskill management and staff.",
+      },
+    ],
+  },
+];
